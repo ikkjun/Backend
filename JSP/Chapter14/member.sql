2907e829059796ee9f568dc2efefc08436dcba52
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-03-24 09:58:58 +0900

    Update member.sql

--- JSP/Chapter14/member.sql
+++ JSP/Chapter14/member.sql
@@ -7,5 +7,22 @@ create table MEMBER (
 
 insert into member(memberid, password, name) values('ikjun96', 'whdlrwns96', '조익준');
 update member set email='ikjun96@naver.com' where memberid='ikjun96';
-insert into member values('miseon95', 'ikjun960722', '김미선', 'miseon95@naver.com');
+insert into member values('miseon95', 'ikjun96722', '김미선', 'miseon95@naver.com');
+
 select * from member;
+
+create table member_history (
+	memberid varchar(10) not null primary key
+    , history long varchar
+)
+default character set = utf8;
+
+insert into member_history values('ikjun96', 
+	concat(
+		'2015 스프링4 프로그래밍입문<br>'
+        , '2013 Spring4.0프로그래밍<br>'
+        , '2012 객체 지향과 디자인패턴<br>'
+        , '2012 JSP 2.2웹프로그래밍\n'
+	)
+);
+select * from member_history;
